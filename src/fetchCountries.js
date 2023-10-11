b5c50cf798f37b95ed2632dc3a13927b757ca405
LacMichal <129613552+LacMichal@users.noreply.github.com>
2023-10-11 09:37:02 +0200

    fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,26 +1,8 @@
-// odniesienia do elementów HTML
+export default fetchCountries;
 
-const fetchUsersBtn = document.querySelector("#search-box");
-const userList = document.querySelector(".country-list");
+const BASE_URL = "https://restcountries.com/v3.1/name";
 
-// zapytanie do API o dane krajów
-
-async function fetchCountries(searchTerm) {
-    const baseUrl = "https://restcountries.com/v3.1/all";
-    const response = await fetch(baseUrl);
-    return response.json();
-  }
-  // generuje kod HTML na podstawie danych krajów 
-
-function renderUserListItems(users) {
-  const markup = users
-    .map(
-      (user) => `<li class="item">
-        <p><b>Name</b>: ${user.name}</p>
-        <p><b>Email</b>: ${user.email}</p>
-        <p><b>Company</b>: ${user.company.name}</p>
-      </li>`
-    )
-    .join("");
-  userList.innerHTML = markup;
+function fetchCountries(name) {
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => response.json())
 }
