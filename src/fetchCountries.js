c4a7782ddf51caf0f754ce2f521447d73e3fff8c
LacMichal <129613552+LacMichal@users.noreply.github.com>
2023-10-12 07:21:59 +0200

    fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,15 @@
-
 const BASE_URL = "https://restcountries.com/v3.1/name";
 
-function fetchCountries(name) {
+export function fetchCountries(name) {
     return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json())
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(`Failed to download country data for ${name}`);
+            }
+        })
+        .catch(error => {
+            throw new Error(error);
+        });
 }
